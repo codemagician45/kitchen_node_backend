e877c37ae16715f6025b2f389f00394807d010e2
asimmurat17@gmail.com <11_Murat_11>
2020-08-08 19:07:46 +0300

    uploadDocuments fix

--- controllers/adminDashboard.js
+++ controllers/adminDashboard.js
@@ -162,7 +162,7 @@ adminDashboard.post("/uploadDocuments",auth,multer.upload.array("files[]"),async
         }
     })
     filePaths = JSON.parse(offerInfo[0].files);
-    console.log(filePaths[0])
+
     for (let i=0;i<req.files.length;i++) {
         fs.renameSync(req.files[i].path, folder_name+"/new/"+req.files[i].filename)
         filePaths.push(folder_name+"/new/"+req.files[i].filename)

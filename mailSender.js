423793f2cc0ec2e852b57d12297b1c939c33ff5a
asimmurat17@gmail.com <11_Murat_11>
2020-08-27 00:29:45 +0300

    mailSender for register logg

--- mailSender.js
+++ mailSender.js
@@ -1,14 +1,23 @@
 /////mail creds
 const nodemailer = require('nodemailer');
 let mailTransporter = {
-    service:"Gmail",
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: 'asimmurat17@gmail.com',
         pass: '11_Murat_1105'
     }
+    /*service:"Gmail",
+    auth: {
+        user: 'asimmurat17@gmail.com',
+        pass: '11_Murat_1105'
+    }
+    */
 }
 
 function mailSend(message) {
+
     let transport = nodemailer.createTransport(mailTransporter);
     let error = false;
     transport.sendMail(message, function (err, info) {
